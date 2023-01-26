d9d16e8d04976f695a7bc33bbf05b867f86cbe5e
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2023-01-26 23:35:21 +0100

    Adds renderAsync method & support for marked async walkTokens option

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const marked = require('marked').marked
+const {marked} = require('marked')
 
 const transformer = {
   name: 'marked',
@@ -7,7 +7,21 @@ const transformer = {
 }
 
 transformer.render = function (string, options) {
-  return marked(string, options)
+  return marked(string, { ...options, async: false })
+}
+
+// Marked doesn't really support async rendering, but this enables async walkTokens extensions,
+// see https://marked.js.org/using_pro#async
+transformer.renderAsync = function (string, options) {
+  const rendered = marked(string, {
+    ...options,
+    async: true,
+  })
+  if (rendered instanceof Promise) {
+    return rendered
+  }
+
+  return Promise.resolve(rendered)
 }
 
 module.exports = transformer
